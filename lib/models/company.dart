51260b8ebcd5daee222b65069ff1382602a107bf
ingvolow <73110438+ingvilow@users.noreply.github.com>
2022-05-04 12:31:23 +0300

    Работа с элементари + достала из списка объекты из АПИ (#2)

---
+++ lib/models/company.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'company.g.dart';
+
+@JsonSerializable()
+class Company {
+  final String name;
+  final String catchPhrase;
+  final String bs;
+
+  Company({
+    required this.name,
+    required this.catchPhrase,
+    required this.bs,
+  });
+
+  factory Company.fromJson(Map<String, String> json) => _$CompanyFromJson(json);
+
+  Map<String, Object?> toJson() => _$CompanyToJson(this);
+}
---
+++ lib/widgets/users_list_models.dart
@@ -0,0 +1,18 @@
+import 'package:elementary/elementary.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:test_provider_2/models/user.dart';
+import 'package:test_provider_2/service/user_service.dart';
+
+//сервис для того, чтобы забрать из АПИ пользователей
+class UsersListModel extends ElementaryModel {
+  final ValueNotifier<List<Users>?> searchUsers = ValueNotifier([]);
+  final ApiService _apiService;
+
+  UsersListModel(
+    this._apiService,
+  );
+
+  Future<List<Users>?> getUser() async {
+    return _apiService.fetchHero();
+  }
+}
