6deaaf3d2033dca87f8acdcbb1b1b7bb9c9cd80c
aedthuill <samosh3223@gmail.com>
2022-05-04 12:00:47 +0300

    полностью все сделано

---
+++ lib/models/geo.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'geo.g.dart';
+
+@JsonSerializable()
+class Geo {
+  final String lat;
+  final String lng;
+
+  Geo({
+    required this.lat,
+    required this.lng,
+  });
+
+  factory Geo.fromJson(Map<String, String> json) => _$GeoFromJson(json);
+
+  Map<String, Object?> toJson() => _$GeoToJson(this);
+}
