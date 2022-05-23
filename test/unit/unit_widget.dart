298cb7a23bb33a245327086a9b7b7debc4fbb2e0
aedthuill <samosh3223@gmail.com>
2022-05-23 13:30:44 +0300

    SPO-13135 юнит тесты для функции

--- test/unit/unit_widget.dart
+++ test/unit/unit_widget.dart
@@ -12,33 +12,38 @@ void main() {
   });
 
   test('validation for less than 18', () {
-    final DateTime time;
-    final underAge = DateTime(time.now)
-    if (notUnderage.compareTo(underage) == 0) {
+    final now = DateTime.now();
+    final maxSupportedDate = DateTime(now.year - 18, now.month, now.day);
+    if (maxSupportedDate.isAfter(now)) {
       const value = birthdayValidation;
       expect(value, StringRes.openBankCardBirthdayTooYoung);
     }
   });
 
   test('if 18 is today', () {
-    final input = DateTime(2005, 9, 7);
-    final date = DateTime.now();
-    final result = input.compareTo(date) == 0;
-    expect(result, isFalse);
+    final now = DateTime.now();
+    final nowDateTime = DateTime(23 - 05 - 2022);
+    if (nowDateTime.isAtSameMomentAs(now)) {
+      const value = birthdayValidation;
+      expect(value, 'birthday is today');
+    }
   });
 
-  test('check validation id datetime is not correct', () {
+  test('check validation is datetime is not correct', () {
     const input = '30-02-2022';
     if (DateTime.tryParse(input) != null) {
       final date = DateFormat('MM-dd-yyyy').format(DateTime.parse(input));
-      expect(date, isTrue);
+      const value = birthdayValidation;
+      expect(date, value);
     }
   });
 
   test('if the value is out of bound the acceptable value', () {
-    const inputFirst = '2022-02-20';
-    const inputSecond = '2022-02-30';
-    final result = inputFirst.compareTo(inputSecond) == 1;
-    expect(result, isFalse);
+    final nowDateTime = DateTime(23 - 05 - 2022);
+    final boundDateTime = DateTime(32 - 05 - 2022);
+    if (nowDateTime.compareTo(boundDateTime) == 0) {
+      const value = birthdayValidation;
+      expect(value, 'out of bound');
+    }
   });
 }
