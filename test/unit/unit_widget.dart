66a17cf5bc3c1ffbdccb9daafe99a52be85819c2
aedthuill <samosh3223@gmail.com>
2022-05-24 14:36:57 +0300

    no-task поправила описание функций теста https://www.notion.so/unit-733a0a1769bf446095e8d2b2e362e515

--- test/unit/unit_widget.dart
+++ test/unit/unit_widget.dart
@@ -10,22 +10,25 @@ void main() {
     expect(value, StringRes.openBankCardBirthdayEmpty);
   });
 
-  test('validation for less than 18', () {
+  test('test for checking if user age is less than 18', () {
     final value = birthdayValidation('20.01.2000', dateTime);
     expect(value, StringRes.openBankCardBirthdayTooYoung);
   });
 
-  test('today is 18', () {
+  test('test for checking if today is 18', () {
     final value = birthdayValidation('07.09.1999', dateTime);
     expect(value, null);
   });
 
-  test('if the value is out of bound the acceptable value', () {
-    final value = birthdayValidation('35.01.2000', dateTime);
-    expect(value, StringRes.openBankCardBirthdayOutOfRange);
-  });
+  test(
+    'test for checking if the value is out of bound the acceptable value',
+    () {
+      final value = birthdayValidation('35.01.2000', dateTime);
+      expect(value, StringRes.openBankCardBirthdayOutOfRange);
+    },
+  );
 
-  test('диапазон 1920г. - по наст.вр', () {
+  test('valid date range testing from 1920 - present', () {
     final value = birthdayValidation('31.12.1919', dateTime);
     expect(value, StringRes.openBankCardBirthdayOutOfRange);
   });
